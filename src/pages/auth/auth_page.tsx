628e9055ac7010f283199bd2378a93d41cea5533
meVarensev <vitalik.unit@gmail.com>
2023-07-14 17:27:22 +0500

    feat: pages main auth error

---
+++ src/pages/auth/auth_page.tsx
@@ -0,0 +1,10 @@
+import {AuthenticationForm} from "../../widgets/auth";
+const AuthPage = () => {
+    return (
+        <div className="auth_container">
+            <AuthenticationForm/>
+        </div>
+    );
+};
+
+export {AuthPage};
---
+++ src/pages/main/main_page.tsx
@@ -0,0 +1,11 @@
+import {Navbar} from "../../widgets/nav_bar";
+
+const MainPage = () => {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    );
+};
+
+export { MainPage };
